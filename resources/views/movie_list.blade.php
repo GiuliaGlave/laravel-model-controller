2d073c43c828e2cc66ce72a9c2599d914022ba7a
GiuliaGlave <giulia.glave@gmail.com>
2023-03-29 18:26:35 +0200

    add database + dd check

---
+++ resources/views/movie_list.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('page-name')
+Movie List
+@endsection
+
+@section('movie-list')
+Movie List content
+@endsection
+
+
+
+{{-- @section('comic-list')
+<div class="comic-list">
+    <div class="position-relative">
+    <span class="label fs-1">CURRENT SERIES</span>
+  </div>
+
+  <section class="pt-5">
+    <ul class="my-5">
+        @foreach ($comic_list as $comic)
+      <li class="mb-4">
+        <a href="#">
+          <div class="cover">
+            <img
+            
+             src=' https://picsum.photos/300/500'
+              alt=""
+            />
+          </div>
+          <div class="series">{{$comic['series']}}</div>
+        </a>
+      </li>
+      @endforeach
+    </ul>
+    <div class="d-flex justify-content-center mb-4">
+      <button class="label">LOAD MORE</button>
+    </div>
+  </section>
+</div>
+@endsection --}}
