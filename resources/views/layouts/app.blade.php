65529a12b67dc828405bfb10c5bdd5d6b4d3d79f
GiuliaGlave <giulia.glave@gmail.com>
2023-04-04 15:08:41 +0200

    add card layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
     @vite('resources/js/app.js')
 </head>
-<body>
+<body class="bg-dark">
 
      @yield('movie-list')
    
--- resources/views/movie_list.blade.php
+++ resources/views/movie_list.blade.php
@@ -6,20 +6,27 @@ Movie List
 
 @section('movie-list')
 
-<section class="pt-5 ">
-    <ul class="my-5">
-        @foreach ($movies as $movie)
-      <li class="mb-4">
-        <a href="#">        
-          <div class="series">{{$movie->title}}</div>
-          <div class="series">{{$movie->original_title}}</div>
-          <div class="series">{{$movie->nationality}}</div>
-          <div class="series">{{$movie->date}}</div>
-          <div class="series">{{$movie->vote}}</div>
-        </a>
-      </li>
-      @endforeach
-    </ul>
+<div class="container my-5">
+  <div class="row row-cols-4 g-3">
+
+  @foreach ($movies as $movie)
+  <div class="col">
+    <div class="card ">
+      <div class="card-body d-flex justify-content-between flex-column">
+        <h5 class="card-title">{{$movie->title}}</h5>
+        <p class="card-text">{{$movie->original_title}}</p>
+        <p class="card-text"><small>{{$movie->date}}</small></p>
+        <p class="card-text d-flex justify-content-end "><small class="text-muted">{{$movie->nationality}}</small></p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted d-flex justify-content-center">voto: {{$movie->vote}}</small>
+      </div>
+    </div>
+  </div>
+   @endforeach
+  
+  </div>
+</div>
 @endsection
 
 
